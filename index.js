227bd51e54b8c44357ff4f184a40a6e20a5dbb50
7u4bgr <abdllyevv@gmail.com>
2023-08-27 14:52:20 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// let users = ["Elcin","Aysel","Reshad","Saida","Said","Solmaz","Oqtay","Turkana","Mahir","Emilia","Samil","Sergiyye","Ceyhun","Benovse","Dadas","Lale","Narmina","Zuleyxa","Taleh","Vusale","Hesen","Ulya","Fuad","Xadice","Qosqar","Gulsen","Ilham","Kemale"];
+// users.sort()
+// console.log(users);
+
+// let num = [100,89,45,32,57,10,91,122,120,112,26,74,66];
+// num.sort((a,b)=>{
+//     return a-b;
+// });
+// console.log(num)
+
+// let users=["Elchin","Aysel","Saida"];
+// let l=users.length;
+// console.log(l);
+
+// let users=["Elchin","Aysel","Saida"];
+// users.shift();
+// console.log(users);
+
+// let users=["Elchin","Aysel","Saida"];
+// users.pop()
+// console.log(users);
+
+
+// let users=["Elchin","Aysel","Saida"];
+// users.unshift("Oqtay");
+// console.log(users);
+
+
+// let users=["Elchin","Aysel","Saida"];
+// delete users[2];
+// console.log(users);
+
+
+// let men=["Elchin","Oqtay","Ali"];
+// let women=["Fatima","Aysel","Saida"];
+// let users=men.concat(women);
+// console.log(users);
